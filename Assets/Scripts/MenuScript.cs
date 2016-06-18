a0d4bfc9a7fd5b219114fb09bce241009646251b
TaneliR <marcu@dnainternet.net>
2016-06-18 23:32:47 +0300

    Start Menu Play button functionality

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour 
+{	
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
