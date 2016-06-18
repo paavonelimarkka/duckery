e788358dc80ea45666c130eaf4692bab2f25f9a5
TaneliR <marcu@dnainternet.net>
2016-06-18 15:48:04 +0300

    Liikkumista muokattu, ankat seuraa ehkä mutsia jo

--- Assets/Scripts/ducklingMovement.cs
+++ Assets/Scripts/ducklingMovement.cs
@@ -5,9 +5,11 @@ public class ducklingMovement : MonoBehaviour {
 
     public Transform goal;
     public float range = 10.0f;
+	public bool seuraa = false;
+	public GameObject duck;
     bool RandomPoint(Vector3 center, float range, out Vector3 result)
     {
-        for (int i = 0; i < 10; i++)
+    //    for (int i = 0; i < 10; i++)
         {
             Vector3 randomPoint = center + Random.insideUnitSphere * range;
             NavMeshHit hit;
@@ -24,16 +26,25 @@ public class ducklingMovement : MonoBehaviour {
     {
 
     }
-
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "duck") {
+			Debug.Log ("Ankanpoikanen osui ankkaan");
+			seuraa = true;
+			GetComponent<CapsuleCollider> ().enabled = false;
+		}
+	} 
     void Update()
     {
         NavMeshAgent ankka = GetComponent<NavMeshAgent>();
         // 
         Vector3 point;
-        if (RandomPoint(transform.position, range, out point))
-        {
-            Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
-            ankka.destination = point;
-        }
-    }
+		if (RandomPoint (transform.position, range, out point) && seuraa == false) {
+			Debug.DrawRay (point, Vector3.up, Color.blue, 1.0f);
+			ankka.destination = point;
+		}
+			if (seuraa == true) {
+			ankka.destination = duck.gameObject.transform.position - new Vector3(0f,0f,-1f);
+		}
+     }
+    
 }
